b1293c8f4d47d41283acf59df01593caff5cc889
William Shinji <willshinji@gmail.com>
2022-07-18 12:33:22 +0100

    forcing build

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from '../src/aws-exports';
+Amplify.configure(awsconfig);
 
 export default function Home() {
   return (
